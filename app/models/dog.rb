6e5be01cd0ab3d8928f035cbba4f4bd4e91de18d
Adam G Horn <hornadam8@gmail.com>
2020-04-11 16:05:36 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,18 @@
 class Dog
   
-  attr_accessor 
-
+  attr_accessor :name,:breed,:age
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+    
+  
+  def initialize(name,age,breed)
+    @name = name
+    @age = age
+    @breed = breed
+    @@all << self
+  end
 end
